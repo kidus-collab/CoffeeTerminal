4d63cd0aef56172ea7e3c3123a23faeb03009ea3
kidus-collab <guyfreaky894@gmail.com>
2023-10-19 23:23:32 +0300

    page export

---
+++ src/Routes/export.ts
@@ -0,0 +1,6 @@
+export { default as Home } from "./Home";
+export { default as Branches } from "./Branches";
+export { default as About } from "./About";
+export { default as Future } from "./Future";
+export { default as Mission } from "./Mission";
+export { default as Services } from "./Services";
