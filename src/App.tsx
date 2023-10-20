6aa6933dcec8181d024b45a65dec26ebce20c140
kidus-collab <guyfreaky894@gmail.com>
2023-10-20 22:20:47 +0300

    route changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,12 @@
 import React from "react";
 
-import { Routes, Route } from "react-router-dom";
-
 //pages
-import {
-  Home,
-  About,
-  Branches,
-  Future,
-  Mission,
-  Services,
-} from "./Routes/export";
+import { Home } from "./Routes/export";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/Branches" element={<Branches />} />
-        <Route path="/Future" element={<Future />} />
-        <Route path="/Mission" element={<Mission />} />
-        <Route path="/Services" element={<Services />} />
-      </Routes>
+      <Home />
     </>
   );
 }
