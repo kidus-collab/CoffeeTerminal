b35ceb127be4053254a53a8ccd001830ebe1cb29
kidus-collab <guyfreaky894@gmail.com>
2023-10-21 19:12:30 +0300

    fix(responsiveness)

--- src/Components/Branches/BranchCard.tsx
+++ src/Components/Branches/BranchCard.tsx
@@ -1,6 +1,7 @@
-import { Box, Flex, Text, Image } from "@chakra-ui/react";
+import { Box, Icon, Button, Text, Image } from "@chakra-ui/react";
 import React, { FC } from "react";
-import { Font } from "../../utils/constants";
+import { Font, colors } from "../../utils/constants";
+import { FaMapMarked } from "react-icons/fa";
 //images
 import Lafto from "../../utils/images/Branches/Lafto branch 1.png";
 import Lebu from "../../utils/images/Branches/Lebu.png";
@@ -10,74 +11,99 @@ interface BranchCardProps {
   img: any;
   branch: string;
   gate: string;
-  location: string;
+  location: any;
 }
 
 export const lafto: BranchCardProps = {
   img: Lafto,
   branch: "Lafto branch",
   gate: "Gate 1",
-  location: "We are found at lafto taxi tera infrot of mosque",
+  location: "We are found at lafto taxi tera infront of mosque",
 };
 export const lebu: BranchCardProps = {
   img: Lebu,
   branch: "Lebu branch",
   gate: "Gate 2",
-  location: "We are found at lafto taxi tera infrot of mosque",
+  location: "We are found at lafto taxi tera infront of mosque",
 };
 export const kera: BranchCardProps = {
   img: Kera,
   branch: "Kera branch",
   gate: "Gate 3",
-  location: "We are found at lafto taxi tera infrot of mosque",
+  location: "We are found at lafto taxi tera infront of mosque",
 };
 
 const BranchCard: FC<BranchCardProps> = ({ img, branch, gate, location }) => {
   return (
     <Box
-      border="2px"
-      borderColor="black"
+      mb={6}
       w="100%"
       display="flex"
       flexDirection={{
         base: "column",
-        md: branch === "Lebu branch" ? "column-reverse" : "column",
+        md: branch === "Lebu branch" ? "row-reverse" : "row",
       }}
     >
       <Box w={{ base: "100%", md: "50%" }}>
         <Image src={img} w="100%" />
       </Box>
-      <Box
-        alignItems="left"
-        w={{ base: "100%", md: "50%" }}
-        justifyContent={branch === "Lebu branch" ? "right" : "left"}
-      >
-        <Box justifyContent="left">
+      <Box w={{ base: "100%", md: "50%" }}>
+        <Box
+          alignItems="left"
+          justifyContent="left"
+          ml={3}
+          my={{ base: 5, md: 3 }}
+        >
           <Text
-            fontSize={{ base: "32px", md: "40px" }}
+            fontSize={{ base: "24px", sm: "28px", md: "30px" }}
             fontWeight="black"
             fontFamily={Font.heading}
-            textAlign="center"
+            textAlign="left"
+            ml={3}
           >
             {branch}
           </Text>
           <Text
-            my={3}
-            fontSize={{ base: "15px", md: "18px" }}
+            mt="-2px"
+            mb={1}
+            fontSize={{ base: "16px", sm: "18px", md: "20px" }}
             fontWeight="bold"
             fontFamily={Font.heading}
-            textAlign="center"
+            textAlign="left"
+            ml={3}
           >
             {gate}
           </Text>
-          <Text
-            fontSize={{ base: "11px", md: "14px" }}
-            fontWeight="light"
-            fontFamily={Font.paragraph}
-            textAlign="center"
+          <Box w={{ base: "64%", sm: "62%", md: "64%" }}>
+            <Text
+              fontSize={{ base: "13px", sm: "14px", lg: "16px" }}
+              fontWeight="light"
+              fontFamily={Font.paragraph}
+              textAlign="left"
+              ml={3}
+            >
+              {location}
+            </Text>
+          </Box>
+          <Button
+            role="group"
+            rounded="xl"
+            px={{ base: 5, lg: 6 }}
+            py={{ base: 3, lg: 6 }}
+            bgColor={colors[1]}
+            alignSelf={{ base: "start", md: "end" }}
           >
-            {location}
-          </Text>
+            <Icon color="white" as={FaMapMarked} />
+            <Text
+              color="white"
+              fontFamily={Font.heading}
+              fontSize={{ base: "10px", sm: "12px", md: "14px", lg: "16px" }}
+              ml={2}
+              fontWeight="bold"
+            >
+              See Map
+            </Text>
+          </Button>
         </Box>
       </Box>
     </Box>
