7ff63b00b411c3deb113e782c0be47e697e01692
kidus-collab <guyfreaky894@gmail.com>
2023-10-21 19:15:55 +0300

    feat: hamburger menu

--- src/Components/Services/MenuCard.tsx
+++ src/Components/Services/MenuCard.tsx
@@ -11,18 +11,43 @@ interface MenuCardProps {
 
 const MenuCard: FC<MenuCardProps> = ({ img, hone, htwo }) => {
   return (
-    <Box w="300px" h="400px" bgColor="white" rounded="xl" boxShadow="lg">
-      <Box rounded="xl" mt={2} w="80%" mx="auto" h="200px" bgImg={img} my="2" />
-      <Box mx="30px" alignItems="left" justifyContent="left">
+    <Box
+      w={{ base: "30%", lg: "25%" }}
+      h={{ base: "160px", sm: "200px", md: "270px", lg: "390px" }}
+      bgColor="white"
+      rounded="2xl"
+      boxShadow="xl"
+    >
+      <Box
+        rounded="xl"
+        mt={2}
+        w={{ base: "90%", lg: "93%" }}
+        mx="auto"
+        h={{ base: "70px", sm: "90px", md: "120px", lg: "200px" }}
+        bgImg={img}
+        bgPos="center"
+        bgSize="cover"
+        bgRepeat="no-repeat"
+        my="2"
+      />
+      <Box
+        mx={{ base: 2, md: 4, lg: "30px" }}
+        alignItems="left"
+        justifyContent="left"
+      >
         <Text
-          mb={2}
+          mb={1}
           fontWeight="extrabold"
-          fontSize="25px"
+          fontSize={{ base: "10px", sm: "15px", md: "20px", lg: "25px" }}
           fontFamily={Font.heading}
         >
           {hone}
         </Text>
-        <Text fontWeight="normal" fontSize="14px" fontFamily={Font.paragraph}>
+        <Text
+          fontWeight="light"
+          fontSize={{ base: "6px", sm: "8px", md: "10px", lg: "14px" }}
+          fontFamily={Font.paragraph}
+        >
           {htwo}
         </Text>
       </Box>
