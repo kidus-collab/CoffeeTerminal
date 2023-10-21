5267143f37e98880d0583e9accb0888c0882cfbd
kidus-collab <guyfreaky894@gmail.com>
2023-10-22 00:13:50 +0300

    neat code

--- src/Components/Branches/BranchCard.tsx
+++ src/Components/Branches/BranchCard.tsx
@@ -1,6 +1,6 @@
 import { Box, Icon, Button, Text, Image } from "@chakra-ui/react";
-import React, { FC } from "react";
-import { Font, colors } from "../../utils/constants";
+import React, { FC, useState } from "react";
+import { Font, colors, laftoMap } from "../../utils/constants";
 import { FaMapMarked } from "react-icons/fa";
 //images
 import Lafto from "../../utils/images/Branches/Lafto branch 1.png";
@@ -24,16 +24,39 @@ export const lebu: BranchCardProps = {
   img: Lebu,
   branch: "Lebu branch",
   gate: "Gate 2",
-  location: "We are found at lafto taxi tera infront of mosque",
+  location: "We are found at lebu around varnero roundabout",
 };
 export const kera: BranchCardProps = {
   img: Kera,
   branch: "Kera branch",
   gate: "Gate 3",
-  location: "We are found at lafto taxi tera infront of mosque",
+  location: "We are found at kera Cherkos road infront of cherkos taxi",
+};
+
+interface locationLinkType {
+  lafto: string;
+  lebu: string;
+  kera: string;
+}
+export const locationLink: locationLinkType = {
+  lafto: laftoMap,
+  lebu: laftoMap,
+  kera: "https://maps.app.goo.gl/yVDXZxur3FdSoxqg8",
 };
 
 const BranchCard: FC<BranchCardProps> = ({ img, branch, gate, location }) => {
+  const [link, setLink] = useState<string | undefined>();
+
+  function locationLinking(): string | undefined {
+    switch (link) {
+      case "Lafto branch":
+        return locationLink.lafto;
+      case "Lebu branch":
+        return locationLink.lebu;
+      case "Kera branch":
+        return locationLink.kera;
+    }
+  }
   return (
     <Box
       mb={6}
@@ -85,25 +108,28 @@ const BranchCard: FC<BranchCardProps> = ({ img, branch, gate, location }) => {
               {location}
             </Text>
           </Box>
-          <Button
-            role="group"
-            rounded="xl"
-            px={{ base: 5, lg: 6 }}
-            py={{ base: 3, lg: 6 }}
-            bgColor={colors[1]}
-            alignSelf={{ base: "start", md: "end" }}
-          >
-            <Icon color="white" as={FaMapMarked} />
-            <Text
-              color="white"
-              fontFamily={Font.heading}
-              fontSize={{ base: "10px", sm: "12px", md: "14px", lg: "16px" }}
-              ml={2}
-              fontWeight="bold"
+          <a href={locationLinking()} target="blank">
+            <Button
+              role="group"
+              rounded="xl"
+              px={{ base: 5, lg: 6 }}
+              py={{ base: 3, lg: 6 }}
+              bgColor={colors[1]}
+              alignSelf={{ base: "start", md: "end" }}
             >
-              See Map
-            </Text>
-          </Button>
+              <Icon color="white" as={FaMapMarked} />
+              <Text
+                color="white"
+                fontFamily={Font.heading}
+                fontSize={{ base: "10px", sm: "12px", md: "14px", lg: "16px" }}
+                ml={2}
+                fontWeight="bold"
+                onClick={() => setLink(branch)}
+              >
+                See Map
+              </Text>
+            </Button>
+          </a>
         </Box>
       </Box>
     </Box>
